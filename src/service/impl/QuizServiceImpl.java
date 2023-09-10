a73b1b1f143fc4ec3fcd8f7bfb158b753091bd87
dormushev <orm_das@mail.ru>
2023-09-10 11:43:38 +0600

    11

--- src/service/impl/QuizServiceImpl.java
+++ src/service/impl/QuizServiceImpl.java
@@ -3,9 +3,8 @@ package service.impl;
 import service.DbHelper;
 import service.QuizService;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.sql.SQLOutput;
+import java.util.*;
 
 public class QuizServiceImpl implements QuizService {
 
@@ -15,15 +14,40 @@ public class QuizServiceImpl implements QuizService {
     public void start() {
         dbHelper.getMap();
         int correctAnswer = 0;
-        int 
+
 
         for (int i =0; i< 5; i++) {
             HashMap<String,HashMap<String,Boolean>> option = dbHelper.getQuestion();
+
+            ArrayList<String> cities = new ArrayList<>();
             for (Map.Entry<String,HashMap<String,Boolean>> item: option.entrySet()){
-                System.out.println("Угадайте столицу страны: "+item.getKey());
+                System.out.println("\tВопрос №"+(i+1)+"\nУгадайте столицу страны: "+item.getKey());
+                for (Map.Entry<String,Boolean> city: item.getValue().entrySet()){
+                    cities.add(city.getKey());
+
+                }
+                Collections.shuffle(cities);
+                System.out.println("Варианты ответов: ");
+                for(String capitals: cities){
+                    System.out.println(capitals);
+
+                }
+                String answer = scanner.next();
+
+//                HashMap<String,Boolean>capitalAndAnswer = item.getValue();
+//                Boolean corrctAnswer = capitalAndAnswer.get(answer);
+                //TODO Обработать ошибку
+
+                if(item.getValue().get(answer)){
+                    correctAnswer++;
+                }
+
             }
 
         }
+        int result = correctAnswer * 100 / 5;
+        System.out.println("Правильных ответов "+correctAnswer);
+        System.out.println("Процент правильных ответов "+result);
 
     }
 }
